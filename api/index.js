fed91146d4084018bad237e79666f077583fdc9d
lakkshyag <lakkshyag@gmail.com>
2024-07-11 11:01:43 +0530

    signup api routes + duplicate key error

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 )
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
         console.log('Server is running on port 3000s');
@@ -19,3 +22,4 @@ app.listen(3000, () => {
 );
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
