2690ebeee84c7f9d13aed1563689df1053933afa
lakkshyag <lakkshyag@gmail.com>
2024-07-26 15:19:01 +0530

    added create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -18,12 +19,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-        console.log('Server is running on port 3000s');
+        console.log('Server is running on port 3000');
     }
 );
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
