8cc6d86bbd5eb3ee1f804c36fa72500feb4e8606
lakkshyag <lakkshyag@gmail.com>
2024-07-30 17:59:01 +0530

    about page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About</h1>
+
+      <p className='mmb-4 text-slate-700'>
+        UnrealEstates aims to help its clients buy, sell, and rent properties in the most desirable neighborhoods. Our primary goal is to make discovering households an undemanding task.
+      </p>
+
+      <br/>
+
+      <p className='mmb-4 text-slate-700'>
+        My main aim for making this website was learning basic CRUD operations and implementing them using MERN.
+      </p>
+
     </div>
   )
 }
